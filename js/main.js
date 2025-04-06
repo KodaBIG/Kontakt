84666d22a43da9352a3df6002c4ca40286561098
KODA <plciastkopl@gmail.com>
2025-04-06 11:24:41 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
 // funkcja strzałkowa
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
+
+	navBtnBars.classList.remove('black-bars-color');
 
 	allNavItems.forEach((item) => {
 		item.addEventListener('click', () => {
@@ -26,4 +32,34 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+// zmienia rok na aktualny na footerze
+handleCurrentYear();
+
 navBtn.addEventListener('click', handleNav);
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+	// ta zmienna będzie przechowywać ile zeskrolowaliśmy
+
+	allSections.forEach((section) => {
+		if (
+			section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.add('black-bars-color');
+		} else if (
+			!section.classList.contains('white-section') &&
+			section.offsetTop <= currentSection + 60
+		) {
+			navBtnBars.classList.remove('black-bars-color');
+		}
+	});
+};
+
+// burger btn zmienia kolor podczas skrolowania te 60 bo wcięcia mamy
+
+window.addEventListener('scroll', handleObserver);
